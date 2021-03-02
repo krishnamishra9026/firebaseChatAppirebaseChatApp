1e8339651cbf65770ed8cdf109ae38f6a42ec146
krishna mishra <er.krishna.mishra@gmail.com>
2021-03-03 00:57:26 +0530

    first commit

---
+++ packages/MyVendor/contactform/src/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+Route::group(['namespace' => 'MyVendor\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
+	Route::get('contact', 'ContactFormController@index');
+	Route::post('contact', 'ContactFormController@sendMail')->name('contact');
+});
+
+?>
